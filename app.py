213c3f7b619d9424f9cd841c09b9f893dda3a993
DM <dmq@MacBook-Air-DM.local>
2026-03-29 12:38:21 +0300

    fix render port

--- app.py
+++ app.py
@@ -18,4 +18,6 @@ def index():
     return render_template("index.html", data=data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
